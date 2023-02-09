6788c6f9ccd707a55ee5e0bf618c87d54f782dd5
m.danilov <vip.maksmax@gmail.com>
2023-02-09 12:56:55 +0400

    removed delays (time.sleep) added construction try/finally

--- test_1.2_11.py
+++ test_1.2_11.py
@@ -3,20 +3,14 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-time.sleep(5)
 
-driver.get("https://suninjuly.github.io/text_input_task.html")
-time.sleep(5)
+try:
+    driver.get("https://suninjuly.github.io/text_input_task.html")
+    textarea = driver.find_element(By.CSS_SELECTOR, ".textarea")
+    textarea.send_keys("get()")
+    submit_button = driver.find_element(By.CSS_SELECTOR, ".submit-submission")
 
-
-textarea = driver.find_element(By.CSS_SELECTOR, ".textarea")
-
-textarea.send_keys("get()")
-time.sleep(5)
-
-submit_button = driver.find_element(By.CSS_SELECTOR, ".submit-submission")
-
-submit_button.click()
-time.sleep(5)
-
-driver.quit()
+    submit_button.click()
+    time.sleep(5)
+finally:
+    driver.quit()
